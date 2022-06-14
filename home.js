328cc5085420633815c1561e487cc5751610c70e
sunwanchung <sunwanchung@gmail.com>
2022-06-14 14:54:28 +0900

    크아아아아악

--- home.js
+++ home.js
@@ -1,10 +1,11 @@
 const icon = document.querySelector('.icon')
-const item1 = document.querySelector('item1')
-
+const item1 = document.querySelector('.item1')
+console.log(item1)
 icon.addEventListener('click',function(){
   window.location = 'https://supersmc1.github.io/project-reborn/'
 })
 
 item1.addEventListener('click',function(){
+  console.log('hello')
   window.location = 'https://supersmc1.github.io/project-News.reborn/'
 })
